e2834d71e0e5c5dbd9a1d54439ef8fbc00e7ced4
Dave <>
2019-11-04 00:39:13 -0500

    Adding repo

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find all dot files then if the original file exists, create a backup
+# Once backed up to {file}.dtbak symlink the new dotfile in place
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+
+echo "Installed"
